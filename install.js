3be844ede5b44962bb82eb50a3ac2fb6ef5b39c5
Monte Hayward <monte@us.ibm.com>
2016-01-23 02:28:24 -0600

    add flvtool2 install

--- install.js
+++ install.js
@@ -1,18 +1,39 @@
-var tarball = require('tarball-extract')
-var request = require('request')
-var fs = require('fs')
+var tarball = require('tarball-extract');
+var request = require('request');
+var fs = require('fs');
+var unzip = require('unzip');
+var flvtool2_zip = 'flvtool2.master.zip';
+
+function rethrowAny(err){
+  if (err) {
+    throw err;
+  }
+}
+
 
 request
-  .get('https://github.com/konsumer/easy-ffmpeg/releases/download/0.0.8/ffmpeg.tgz')
-  .on('error', function (err) {
-    throw err
-  })
-  .pipe(fs.createWriteStream('ffmpeg.tgz'))
-  .on('close', function () {
-    tarball.extractTarball('ffmpeg.tgz', __dirname, function (err, result) {
-      if (err) throw err
-      fs.unlink('ffmpeg.tgz', function (err) {
-        if (err) throw err
-      })
+    .get('https://github.com/konsumer/easy-ffmpeg/releases/download/0.0.8/ffmpeg.tgz')
+    .on('error', function(err) {
+      throw err
     })
-  })
+    .pipe(fs.createWriteStream('ffmpeg.tgz'))
+    .on('close', function() {
+      tarball.extractTarball('ffmpeg.tgz', __dirname, function(err, result) {
+        fs.unlink('ffmpeg.tgz', function(err) {
+          rethrowAny(err);
+        })
+      })
+    });
+
+request
+    .get('https://codeload.github.com/unnu/flvtool2/zip/master')
+    .on('error', function(err) {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(flvtool2_zip))
+    .on('close', function() {
+      fs.createReadStream(flvtool2_zip).pipe(unzip.Extract({path: __dirname}));
+      fs.unlink(flvtool2_zip, function(err) {
+        rethrowAny(err);
+      });
+    });
